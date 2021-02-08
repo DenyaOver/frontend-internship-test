dc9b4958a7148bb608ff3afcc651dfa33bdd088d
Denya <dena_holmes@ukr.net>
2021-02-08 14:37:25 +0100

    task 2, 3

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,40 @@
-/* Here goes your JS code */
+
+document.addEventListener("DOMContentLoaded", function(){
+    const form = document.querySelector('form'),
+          close_form = form.querySelector('.close-button'),
+          main = document.querySelector('.main'),
+          show = document.querySelector('#show-popup-form'),
+          message = document.createElement('div');
+
+
+    show.addEventListener('click', ()=>{
+        form.classList.remove("hide");
+        form.classList.add("show");
+        show.classList.add("hide");
+        message.classList.add('btn-main');
+        message.textContent = "Thank you!";
+        main.append(message);
+        message.classList.remove("hide");
+        message.classList.add("show");
+    });
+
+
+    close_form.addEventListener('click', ()=>{
+        form.classList.remove("show");
+        form.classList.add("hide");
+        show.classList.remove("hide");
+        message.classList.remove ("show");
+        message.classList.add("hide");
+    });
+
+    
+    form.addEventListener('submit', (e) =>{
+            e.preventDefault();
+            form.reset();
+            setTimeout(() => {
+                form.classList.remove("show");
+                form.classList.add("hide");
+            }, 3000);
+    });
+       
+});
